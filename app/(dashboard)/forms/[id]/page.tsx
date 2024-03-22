e400adf73a64f96e354a1e2b629a9b352456a584
AshuJoshi001 <47688714+AshuJoshi001@users.noreply.github.com>
2024-03-22 17:26:12 +0530

    First

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -1,13 +1,15 @@
 import { GetFormById, GetFormWithSubmission } from "@/action/form";
 import FormLinkShare from "@/components/FormLinkShare";
 import VisitBtn from "@/components/VisitBtn";
-import React from "react";
+import React, { ReactNode } from "react";
 import {StatsCard} from "../../page";
 import {LuView} from "react-icons/lu";
 import {FaWpforms} from "react-icons/fa";
 import {HiCursorClick} from "react-icons/hi";
 import {TbArrowBounce} from "react-icons/tb";
 import { ElementsType, FormElementInstance } from "@/components/FormElements";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { formatDistance } from "date-fns";
 
 
 
@@ -88,6 +90,10 @@ async function FormDetailPage({params,}:
 
 export default FormDetailPage;
 
+type Row = {[key:string]:string} & {
+  submittedAt: Date;
+} ;
+
 async function SubmissionTable({id}:{id:number}) {
   const form = await GetFormWithSubmission(id);
 
@@ -101,12 +107,82 @@ async function SubmissionTable({id}:{id:number}) {
     label:string;
     required: boolean;
     type: ElementsType
-  }
+  }[]=[];
 
+  formElements.forEach(element=>{
+    switch(element.type){
+      case "TextField":
+        columns.push({
+          id:element.id,
+          label:element.extraAttributes?.label,
+          required:element.extraAttributes?.required,
+          type:element.type
+        });
+        break;
+        default:
+          break;
+    }
+  });
+
+  const rows : Row[] = [];
+  form.FormSubmissions.forEach(submission =>{
+    const content = JSON.parse(submission.content);
+    rows.push({
+      ...content,
+      submittedAt:submission.createdAt,
+    });
+  });
   return (
   <>
   <h1 className="text-2xl font-bold my-4">Submissions</h1>
+  <div className="rounded-md border">
+    <Table>
+      <TableHeader>
+        <TableRow>
+          {columns.map((column)=>(
+            <TableHead key={column.id} className="uppercase">
+              {column.label}
+              </TableHead>
+          ))}
+          <TableHead className="text-muted-foreground text-right uppercase">
+            Submitted At
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {
+          rows.map((row, index) =>(
+            <TableRow key={index}>
+             {
+              columns.map(column =>(
+                <RowCell key={column.id} 
+                type={column.type}
+                value={row[column.id]}
+                />
+              ))}
+              <TableCell className="text-muted-foreground text-right">
+                {
+                  formatDistance(row.submittedAt, new Date(),{
+                    addSuffix: true,
+                  })
+                }
+              </TableCell>
+            </TableRow>
+          ))
+        }
+      </TableBody>
+    </Table>
+  </div>
   </>
+  );  
+}
+
+function RowCell({type, value}:{type: ElementsType; value:string}){
+  let node: ReactNode = value;
+
+  return (
+    <TableCell>
+      {node}
+    </TableCell>
   );
-  
 }
--- components/FormElements.tsx
+++ components/FormElements.tsx
@@ -1,8 +1,27 @@
 import React from "react";
 import { TextFieldFormElement } from "./fields/TextField";
+import { TitleFieldFormElement } from "./fields/TitleField";
+import { SubTitleFieldElement } from "./fields/SubTitleField";
+import { ParagraphFieldFormElement } from "./fields/ParagraphField";
+import { SeparatorFieldFormElement } from "./fields/SeparatorField";
+import { SpacerFieldFormElement } from "./fields/SpacerField";
+import { NumberFieldFormElement } from "./fields/NumberField";
+import { TextAreaFieldFormElement } from "./fields/TextAreaField";
+import { DateFieldFormElement } from "./fields/DateField";
+import { SelectFieldFormElement } from "./fields/SelectField";
 
 
-export type  ElementsType = "TextField";
+export type  ElementsType = 
+"TextField" 
+| "TitleField" 
+| "SubTitleField"
+| "ParagraphField"
+| "SeparatorField"
+| "SpacerField"
+| "NumberField"
+| "TextAreaField"
+| "DateField"
+| "SelectField";
 export type SubmitFunction = (key:string, value:string)=>void;
 export type FormElement = {
     type:ElementsType;
@@ -41,5 +60,14 @@ type FormElementsType = {
 };
 export const FormElements :FormElementsType= {
     TextField:TextFieldFormElement,
+    TitleField:TitleFieldFormElement,
+    SubTitleField: SubTitleFieldElement,
+    ParagraphField: ParagraphFieldFormElement,
+    SeparatorField: SeparatorFieldFormElement,
+    SpacerField: SpacerFieldFormElement,
+    NumberField: NumberFieldFormElement,
+    TextAreaField: TextAreaFieldFormElement,
+    DateField : DateFieldFormElement,
+    SelectField : SelectFieldFormElement,
 };
 
